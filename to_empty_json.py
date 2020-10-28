548c572248a1da33248f1a5391da6b3e0b7d87c0
wanghao <wanghaobjyd@163.com>
2020-10-28 10:22:14 +0800

    change something

--- to_empty_json.py
+++ to_empty_json.py
@@ -7,14 +7,12 @@ import numpy as np
 # import xml.etree.ElementTree as ET
 
 _classes = ('__background__',  # always index 0
-            'codezhengchang', 'capduandian', 'capposun',
-            'capbianxing', 'caphuaibian', 'labelqizhou',
-            'capdaxuan', 'codeyichang', 'labelwaixie', 'labelqipao')
+            'holothurian', 'echinus', 'starfish', 'scallop', 'waterweeds')
 
-data_list = '/Users/wanghao/Desktop/Tianchi_bottle/testA.txt'  # train, val or test txt, No suffix
-img_path = '/Users/wanghao/Desktop/Tianchi_bottle/testA/images/'  # image folder path
+data_list = '/Users/wanghao/Desktop/ocean/test_A/test.txt'  # train, val or test txt, No suffix
+img_path = '/Users/wanghao/Desktop/ocean/test_A/image/'  # image folder path
 # xml_path = '/Users/wanghao/Desktop/Tianchi_bottle/xml/'  # xml folder path
-save_json_path = '/Users/wanghao/Desktop/Tianchi_bottle/testA.json'  # name for save json
+save_json_path = '/Users/wanghao/Desktop/ocean/test_A/test_A.json'  # name for save json
 
 
 class Convert_xml_to_coco(object):
@@ -54,8 +52,7 @@ class Convert_xml_to_coco(object):
             # print data_name
             # XML
             img = cv2.imread(img_path + data_name + '.jpg')
-            cv2.imwrite('/Users/wanghao/Desktop/det/new_images/' + data_name + '.jpg', img)
-            # tree = ET.parse(xml_path + data_name + '.xml')
+
             filename = data_name + '.jpg'
             width = img.shape[1]
             height = img.shape[0]
@@ -66,41 +63,6 @@ class Convert_xml_to_coco(object):
             annotation = {}
             self.annotations = annotation
 
-            # object = tree.findall('object')
-            # for ix, obj in enumerate(object):
-            #     if obj.find('name').text.lower().strip() == 'cloth_hat':
-            #         continue
-            #     else:
-            #         bbox = obj.find('bndbox')
-            #         label = obj.find('name').text.lower().strip()
-            #
-            #     if label not in _classes:
-            #         print(filename, label)
-            #
-            #     try:
-            #         difficult = int(obj.find('difficult').text)
-            #     except:
-            #         difficult = 0
-            #
-            #     # if label == 'none':
-            #     #     print(filename)
-            #     #     continue
-            #
-            #     x1 = np.maximum(0.0, float(bbox.find('xmin').text))
-            #     y1 = np.maximum(0.0, float(bbox.find('ymin').text))
-            #     x2 = np.minimum(width - 1.0, float(bbox.find('xmax').text))
-            #     y2 = np.minimum(height - 1.0, float(bbox.find('ymax').text))
-            #
-            #     # rectangle = [x1, y1, x2, y2]
-            #     bbox = [x1, y1, x2 - x1 + 1, y2 - y1 + 1]  # [x,y,w,h]
-            #     area = (x2 - x1 + 1) * (y2 - y1 + 1)
-            #
-            #     # annotations
-            #     annotation = {'segmentation': [], 'iscrowd': 0, 'area': area, 'image_id': num + 1,
-            #                   'bbox': bbox, 'difficult': difficult,
-            #                   'category_id': self.label_map[label], 'id': self.annID}
-            #     self.annotations.append(annotation)
-            #     self.annID += 1
 
     def save_json(self):
         data_coco = {'images': self.images, 'categories': self.categories, 'annotations': self.annotations}
